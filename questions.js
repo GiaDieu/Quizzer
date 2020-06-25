0883be49c4da64ca52bf2a615c403b9711d9ad66
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-06-25 14:17:44 +0700

    Add_project

---
+++ questions.js
@@ -0,0 +1,21 @@
+var Questions = { "question" : [
+  {
+    "question"  : "What is HTML?",
+     "choice1"  : "HTML",
+     "choice2"  : "HTXL",
+     "choice3"  : "HTML",
+     "choice4"  : "LLLL",
+     "choice5"  : "EEEE",                    
+     "correct"  : 1
+  },
+  
+  {
+    "question"  : "What is CSS?",
+     "choice1"  : "CSS",
+     "choice2"  : "SSC",
+     "choice3"  : "CCD",
+     "choice4"  : "TOO",
+     "choice5"  : "CONCA",                    
+     "correct"  : 4
+  },
+]};
